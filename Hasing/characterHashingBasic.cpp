7ddbfccb04933f24b6d91ba1a9121e4b2cc20cf0
Aditya Singh <adityasinghdasila15@gmail.com>
2024-07-11 15:10:12 +0530

    characterHashingBasic.cpp

---
+++ Hasing/characterHashingBasic.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int hsh[26]={0},index;
+    cout<<"enter the number of elements to enter: ";
+    int n,i=1;
+    char ele;
+    cin>>n;
+    while(i<=n){
+        cin>>ele;
+        index = ele - 'a';
+        hsh[index]++;
+        i++;
+    }
+    cout<<"enter the character you want to search :";
+    cin>>ele;
+    cout<<hsh[ele-'a']<<" is the frequency of "<<ele;
+
+    return 0;
+}
---
+++ Hasing/practice/test.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(){
+    //just testing implicit typecasting done by c++
+    int x='b';
+    char m = 65;
+    cout<<x<<endl;
+    cout<<m<<endl;
+    return 0;
+}
