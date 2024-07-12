68e63818ab5c2ec1589c43fdcfd5c84a7a33aee2
Aditya Singh <adityasinghdasila15@gmail.com>
2024-07-12 13:39:30 +0530

    bubble sort

--- sorting/bubble.cpp
+++ sorting/bubble.cpp
@@ -4,12 +4,16 @@ using namespace std;
 void bubble(vector<int>& v){
     int i=0,j=0;
     for(i=0;i<v.size();i++){
+        int didswap=0;
         for(j=0;j<v.size()-i-1;j++){
             if(v[j]>v[j+1])
             {
                 swap(v[j],v[j+1]);
+                didswap=1;
             }
         }
+        if(didswap==0)
+            break;
     }
 }
 int main(){
@@ -29,3 +33,9 @@ int main(){
 
     return 0;
 }
+
+/*
+    DIDSWAP optimization:
+    if in any iteration, there is no swap in the j loop, this mean that the whole array is already im ascending order. thus no further iterations needed.
+    thus, if didswap is not turned ==1 by jloop, then no swap was done => array sorted => break form i loop.
+*/
