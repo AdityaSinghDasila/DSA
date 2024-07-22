8f09998af5a70b30dbdc8bf60d4de5b6686ded58
Aditya Singh <adityasinghdasila15@gmail.com>
2024-07-22 14:31:00 +0530

    insertion sort practice

---
+++ sorting/practice/insertion_sort.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+void insertion(vector<int>& arr){
+    for(int i=0;i<arr.size();i++){
+        for(int j=i+1;j>0;j--){
+            if(arr[j]<arr[j-1]){
+                swap(arr[j],arr[j-1]);
+            }
+            else    
+                break;
+        }
+    }
+}
+int main(){
+    vector<int> arr= {34,12,34,1,2,5,1,2,5,646,7,6,434,7,8,9,22,34,54,32};
+    
+    cout<<"The vector before sorting : ";
+    for(int i=0;i<arr.size();i++){
+        cout<<arr[i]<<" ";
+    }
+
+    insertion(arr);
+
+    cout<<endl<<"The vector after sorting: ";
+    for(int i=0;i<arr.size();i++){
+        cout<<arr[i]<<" ";
+    }
+
+    return 0;
+}
