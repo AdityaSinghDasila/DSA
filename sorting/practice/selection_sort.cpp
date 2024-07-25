c585acd87b1ead6b8d7d4183cdeb1307fd5f7218
Aditya Singh <adityasinghdasila15@gmail.com>
2024-07-25 11:45:19 +0530

    selection sort practice

--- sorting/practice/selection_sort.cpp
+++ sorting/practice/selection_sort.cpp
@@ -1,3 +1,5 @@
+/*
+
 #include <iostream>
 #include <bits/stdc++.h>
 using namespace std;
@@ -26,6 +28,42 @@ int main(){
     cout<<endl<<"The vector after selection sort : ";
     for(int i=0;i<arr.size();i++){
         cout<<arr[i]<<" ";
+    }
+
+    return 0;
+}
+*/
+
+
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+void selection(vector<int>& arr){
+    //in selection, we grab the minimum element and throw it first
+    for(int i=0;i<arr.size();i++){
+        int min=i;
+        for(int j=i;j<arr.size();j++){
+            if(arr[j]<arr[min]){
+                min=j;
+            }
+        } 
+        swap(arr[i],arr[min]);
+    }
+}
+
+int main(){
+    vector<int> arr ={23,12,2,4,1,54,3,67,97,1452,0,12,2,7,7,9};
+    cout<<"The vector before sorting : ";
+    for(int i:  arr ){
+        cout<<i<<" ";
+    }
+
+    selection(arr);
+    
+    cout<<endl<<"The vector after sorting :";
+    for(int i: arr){
+        cout<<i<<" ";
     }
 
     return 0;
