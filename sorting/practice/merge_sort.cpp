d5d895116fabafde49da352ce2366bffae92e13a
Aditya Singh <adityasinghdasila15@gmail.com>
2024-07-25 11:27:54 +0530

    Merge sort practice

--- sorting/practice/merge_sort.cpp
+++ sorting/practice/merge_sort.cpp
@@ -70,6 +70,7 @@ int main(){
 }
 */
 
+/*
 
 #include <iostream>
 #include <bits/stdc++.h>
@@ -128,6 +129,78 @@ int main(){
     cout<<endl<<"The vector after sorting : ";
     for(int i=0;i<arr.size();i++){
         cout<<arr[i]<<" ";   
+    }
+
+    return 0;
+}
+*/
+
+
+
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+void merge(vector<int>& arr,int first,int middle, int last){
+    //now we have two hypotheically virtual arrays, which are sorted.
+    // one is from first-->middle, the second is from middle+1-->last
+    //we make two pointers left and right and put them on the starting of the two arrays respectively.
+    //since the two arrays are sorted already, we compare the two arrays and then put the smaller element into a temporary vector. When the vector is filled, this merged array is the new bigger sorted array.
+     
+    int left=first, right= middle+1;
+    vector<int> temp;
+    while(left<=middle && right<=last){
+        if(arr[left]<=arr[right]){
+            temp.push_back(arr[left]);
+            left++;
+        }
+        else{
+            temp.push_back(arr[right]);
+            right++;
+        }
+    }
+    //emptying the array that did not get exhausted in the above array
+    while(left<=middle)
+    {
+        temp.push_back(arr[left]);
+        left++;
+    }
+    while(right<=last){
+        temp.push_back(arr[right]);
+        right++;
+    }
+
+    //now the temp array has been filled.
+
+    //transferring the whole temp array[0->size()-1]   into the original array [FIRST-->LAST]
+    for(int i=first;i<=last;i++){
+        arr[i]=temp[i-first];
+    }
+
+    //merge has been done
+}
+void mergeSort(vector<int>& arr,int first, int last){
+    //now lets break them down by recursion
+    if(first==last)
+        return;
+    int middle = (first+last)/2;
+    mergeSort(arr,first,middle);
+    mergeSort(arr,middle+1,last);
+    merge(arr,first,middle,last);
+}
+
+using namespace std;
+int main(){
+    vector<int> arr ={34,12,4,2,12,53,23,45,2,36,78,6,4,82,0,0,5,4,3};
+    cout<<"The vector before sorting :";
+    for(int i : arr){
+        cout<<i<<" ";
+    }
+
+    mergeSort(arr,0,arr.size()-1); //sent the first and last element index
+
+    cout<<endl<<"The vector after sorting :";
+    for(int i: arr){
+        cout<<i<<" ";
     }
 
     return 0;
