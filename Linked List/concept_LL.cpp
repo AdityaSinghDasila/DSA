f2ad3c88e506c061517a5b4f88a0ddc740353e0a
Aditya Singh <adityasinghdasila15@gmail.com>
2024-07-27 14:38:01 +0530

    linked list start

---
+++ Linked List/concept_LL.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+//making a linked list 
+//use class instead of struct because of added functionalities like abstraction, encapsulation etc in class
+int main(){
+
+    return 0;
+}
