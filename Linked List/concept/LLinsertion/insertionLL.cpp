63a860cfa548653ba9278435cc2db83c7d99c111
Aditya Singh <adityasinghdasila15@gmail.com>
2024-07-29 09:56:33 +0530

    practice ll concepts

--- Linked List/concept/LLinsertion/insertionLL.cpp
+++ Linked List/concept/LLinsertion/insertionLL.cpp
@@ -21,9 +21,59 @@ node* insHead(node* head, int el){
 }
 
 node* insert(node* head, int el, int pos){
+    if(head == nullptr){
+        if(pos==1)
+            return new node(el);
+    }
+    if(pos==1){
+        node* newh = new node(el);
+        newh->next = head;
+        head = newh;
+        return head;
+    }
+    int count = 1;
+    node* mover = head;
+    while(count!=pos-1){
+        if(mover==nullptr)
+        {
+            cout<<endl<<" not valid ";
+            return nullptr;
+        }
+        mover=mover->next;
+        count++;
+    }
+    node* newnode = new node(el);
+    newnode->next=mover->next;
+    mover->next = newnode;
+
+    return head;
+}
 
 
-    return nullptr;
+node* insertBeforeEl(node* head, int el, int bef){
+    if(head->data == bef){
+        node* temp = new node(el);
+        temp->next = head;
+        head = temp;
+        return head;
+    }
+    bool found =false;
+    node* temp = head;
+    while(temp->next!=nullptr){
+        if(temp->next->data==bef){
+            node* newnode = new node(el);
+            newnode->next = temp->next;
+            temp->next = newnode;
+            found =1;
+            return head;
+        }
+        temp=temp->next;
+    }
+    if(found==false)
+    {
+        cout<<endl<<"Not present in LL ";
+    }
+    return head;
 }
 
 int main(){ 
@@ -41,23 +91,39 @@ int main(){
         cout<<mover->data<<" ";
         mover=mover->next;
     }
-    
-    int el,pos;
-    cout<<endl<<"Enter the element you want to insert :";
+    /* inserting an element at a position */
+    // int el,pos;
+    // cout<<endl<<"Enter the element you want to insert :";
+    // cin>>el;
+    // cout<<endl<<"Enter the position you want to insert it in :";
+    // cin>>pos;
+
+    // if(pos==1){
+    //     head = insHead(head,el);
+    // }
+    // else{
+    //     head = insert(head,el,pos);
+    // }
+
+    // //now just print the new linked list
+    // mover = head;
+    // cout<<endl<<"The new linked list after insertion is : ";
+    // while(mover!=nullptr){
+    //     cout<<mover->data<<" ";
+    //     mover=mover->next;
+    // }
+
+
+    /* inserting an element before already existing element */
+    int bef,el;
+    cout<<endl<<"Enter the element before which you want to insert : ";
+    cin>>bef;
+    cout<<endl<<"Enter the element : ";
     cin>>el;
-    cout<<endl<<"Enter the position you want to insert it in :";
-    cin>>pos;
+    head = insertBeforeEl(head,el,bef);
 
-    if(pos==1){
-        head = insHead(head,el);
-    }
-    else{
-        head = insert(head,el,pos);
-    }
-
-    //now just print the new linked list
-    mover = head;
     cout<<endl<<"The new linked list after insertion is : ";
+    mover=head;
     while(mover!=nullptr){
         cout<<mover->data<<" ";
         mover=mover->next;
---
+++ Linked List/concept/practice.cpp
@@ -0,0 +1,86 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+class node{
+    public : 
+    int data;
+    node* next;
+    // constructor
+    node(int data1){
+        data=data1;
+        next= nullptr;
+    }
+};
+
+void printLL(node* head){
+    node* mover = head;
+    while(mover!=nullptr){
+        cout<<mover->data<<" ";
+        mover=mover->next;
+    }
+}
+
+node* delHead(node* head){
+    node* temp = head;
+    head = head->next;
+    temp->next=nullptr;
+    delete temp;
+    return head;
+}
+
+node* delPos(node* head, int pos){
+    if(pos==1){
+        return delHead(head);
+    }
+    if(head==nullptr){
+        cout<<endl<<"invalid";
+        return nullptr;
+    }
+    node* mover = head;
+    bool found =false;
+    int count = 0;
+    while(mover->next!=nullptr){
+        count++;
+        if(count==pos-1){
+            node* temp = mover -> next;
+            mover ->next =temp->next;
+            delete temp;
+            found=true;
+            break;
+        }
+        mover=mover->next;
+    }
+    if(found==false)
+    {
+        cout<<"The position you entered is wrong";
+    }
+    return head;
+}
+
+int main(){
+    node* head = new node(0);
+    node* mover = head;
+    for(int i =1; i<=5;i++){
+        node* temp = new node(i);
+        mover->next = temp;
+        mover = temp;
+    }//LL filled
+    cout<<"The linked list initially : ";
+    printLL(head);
+
+    // //delete head
+    // head = delHead(head);
+    // cout<<endl<<"After deleting the head, the linked list: ";
+    // printLL(head);
+
+
+    //delete at kth element starting from 1
+    int pos;
+    cout<<endl<<"ENter the position you want to delete : ";
+    cin>>pos;
+    head = delPos(head,pos);
+    cout<<endl<<"The linked list now : ";
+    printLL(head);
+
+    return 0;
+}
