ddd1d149c7ee22e7bb19bb85a1e0c02d6d4687f7
Aditya Singh <adityasinghdasila15@gmail.com>
2024-07-29 10:30:51 +0530

    practice Deletion

--- Linked List/concept/practiceDel.cpp
+++ Linked List/concept/practiceDel.cpp
@@ -57,6 +57,30 @@ node* delPos(node* head, int pos){
     return head;
 }
 
+node* delEl(node* head, int el){
+    if(head->data==el)
+    {   
+        return delHead(head);
+    }
+    node* mover = head;
+    bool found=false;
+    while(mover->next!=nullptr){
+        if(mover->next->data==el){
+            node* temp = mover->next;
+            mover->next=temp->next;
+            delete temp;
+            found = true;
+            break;
+        }
+        mover = mover->next;
+    }
+    if(found == false)
+    {
+        cout<<"The element is not present in the LL";
+    }
+    return head;
+}
+
 int main(){
     node* head = new node(0);
     node* mover = head;
@@ -68,18 +92,27 @@ int main(){
     cout<<"The linked list initially : ";
     printLL(head);
 
-    // //delete head
+    /* delete head */
     // head = delHead(head);
     // cout<<endl<<"After deleting the head, the linked list: ";
     // printLL(head);
 
 
-    //delete at kth element starting from 1
-    int pos;
-    cout<<endl<<"ENter the position you want to delete : ";
-    cin>>pos;
-    head = delPos(head,pos);
-    cout<<endl<<"The linked list now : ";
+    /* delete at kth element starting from 1 */
+    // int pos;
+    // cout<<endl<<"ENter the position you want to delete : ";
+    // cin>>pos;
+    // head = delPos(head,pos);
+    // cout<<endl<<"The linked list now : ";
+    // printLL(head);
+
+    /* delete element el from the linked list if present */
+    mover = head;
+    int el;
+    cout<<endl<<"Enter the element you want to delete : ";
+    cin>>el;
+    head = delEl(head,el);
+    cout<<endl<<"The linked list :";
     printLL(head);
 
     return 0;
