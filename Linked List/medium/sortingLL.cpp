cec36605f3447dcaecfb904e68556ff148023859
Aditya Singh <adityasinghdasila15@gmail.com>
2024-08-01 19:26:17 +0530

    Sort linked list through merge sort

--- Linked List/medium/sortingLL.cpp
+++ Linked List/medium/sortingLL.cpp
@@ -13,16 +13,65 @@ class node{
     }
 };
 
+node* findMiddle(node* head){
+    //tortoise and hare algo
+    node* fast = head->next->next;
+    node* slow = head;
+    while(fast!=nullptr && fast->next != nullptr){
+        fast = fast->next->next;
+        slow = slow->next;
+    }
+    return slow;
+}
+node* merge2SortedLL(node* head1, node* head2){
+    node* head = new node(-1);
+    node* temp1= head1;
+    node* temp2 = head2;
+    node* mover = head;
+
+    while(temp1!=nullptr && temp2!=nullptr){
+        if(temp1->data<=temp2->data){
+            mover->next = temp1;
+            mover = temp1;
+            temp1 = temp1->next;
+        }
+        else{
+            mover->next = temp2;
+            mover = temp2;
+            temp2 = temp2->next;
+        }
+    }
+    while(temp1!=nullptr){
+        mover->next = temp1;
+        temp1=temp1->next;
+        mover=mover->next;
+    }
+    while(temp2!=nullptr){
+        mover->next = temp2;
+        temp2 = temp2->next;
+        mover = mover->next;
+    }
+    return head->next;
+}
+
 node* mergeSort(node* head){
     if(head==nullptr || head->next == nullptr){
+        // cout<<"haha"<<endl;
         return head;
     }
+    node* middle = findMiddle(head);
+    node* leftHead = head;
+    node* rightHead = middle->next;
+    middle->next = nullptr;
+    leftHead = mergeSort(leftHead);
+    rightHead = mergeSort(rightHead);
+    return merge2SortedLL(leftHead,rightHead);
 }
 
 int main(){
     node* head = new node(0);
     node* mover = head;
-    cout<<"enter elements, enter 100 to stop";
+    cout<<"enter elements, enter 100 to stop : ";
     int n;
     while(n!=100){
         cin>>n;
@@ -30,6 +79,7 @@ int main(){
         mover->next = temp;
         mover = temp;
     }
+    head=head->next;
     cout<<endl<<"The Linked list formed is : ";
     mover =head;
     while(mover!=nullptr){
