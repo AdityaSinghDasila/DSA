a19a9beac5694e9df2e0a167128bad11e5252771
Aditya Singh <adityasinghdasila15@gmail.com>
2024-08-05 12:01:50 +0530

    upper bound of an element in an array through binary search

---
+++ Binary Search/search code/upper_bound.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    vector<int> arr ={1,5,9,11,16,23,34,77,109,255};
+    cout<<endl<<"The array :";
+    for(int i:arr){
+        cout<<i<<" ";
+    }
+    int el;
+    cout<<endl<<"Enter the element you want the upper bound of form the array: ";
+    cin>>el;
+
+    int first= 0;
+    int last = arr.size()-1;
+    int middle = first +(last-first)/2;
+    int ans =-1;
+    while(first<=last){
+        int middle = first +(last-first)/2;
+        if(arr[middle]==el){
+            ans = middle;
+            break;
+        }
+        if(arr[middle]<el){
+            if(middle>ans){
+                ans = middle;
+            }
+            first = middle +1;
+        }
+        else if(arr[middle]>el){
+            last = middle -1;
+        }
+    }
+    
+    if(ans != -1){
+        cout<<endl<<"The upper bound of "<<el<<" was found at index "<<ans<<" which is "<<arr[ans];
+    }else{
+        cout<<"The upper bound of "<<el<<" was not found";
+    }
+
+    return 0;
+}
