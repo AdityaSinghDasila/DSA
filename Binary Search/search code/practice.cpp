9f45dddf9c655738fc2bbfb533d5c8d54df87af2
Aditya Singh <adityasinghdasila15@gmail.com>
2024-08-06 10:33:02 +0530

    practice binary search

--- Binary Search/search code/practice.cpp
+++ Binary Search/search code/practice.cpp
@@ -1,3 +1,5 @@
+/* binary search code */
+/*
 #include <iostream>
 #include <bits/stdc++.h>
 using namespace std;
@@ -37,5 +39,118 @@ int main(){
         cout<<endl<<"The element was not found!!";
     }
     
+    return 0;
+}
+*/
+
+/*  BINARY SEARCH CODE */
+/*
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    vector<int>arr = {1,2,3,4,5,6,7,8,9,10,200};
+    cout<<endl<<"The array : ";
+    for(int i: arr){
+        cout<<i<<" ";
+    }
+
+    int el;
+    cout<<endl<<"Enter the element you want to search : "; 
+    cin>>el;
+
+    int first = 0;
+    int last = arr.size()-1;
+    int ans = -1;
+    int middle = first +(last-first)/2;
+    while(first<=last){
+        middle = first+(last-first)/2;
+        if(arr[middle]==el){
+            ans = middle;
+            break;
+        }
+        if(arr[middle]<el){
+            first=middle+1;
+        }
+        else if(arr[middle]>el){
+            last = middle-1;
+        }
+    }
+    if(ans!=-1){
+        cout<<endl<<"The element was found at index :"<<ans;
+    }else{
+        cout<<endl<<"The element was not found in the array!!";
+    }
+
+    return 0;
+}
+*/
+
+/* lower bound */
+/*
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    vector<int>arr ={1,2,3,4,5,6,7,8,9,10,200};
+    cout<<endl<<"The array :";
+    for(int i: arr){
+        cout<<i<<" ";
+    }
+
+    int el;
+    cout<<endl<<"Enter the element you want to find the upper bound for : ";
+    cin>>el;
+
+    int first =0;
+    int last = arr.size()-1;
+    int ans = arr.size();
+    int middle = first+(last-first)/2;
+    while(first <= last){
+        middle = first+(last-first)/2;
+        if(arr[middle]>=el){
+            ans = middle;
+            last = middle-1;
+        }
+        else if(arr[middle]>el){
+            first = middle+1;
+        }
+    }
+    cout<<endl<<"The upper bound will be at the index : "<<ans; 
+    return 0;
+}
+*/
+
+
+/*The upper bound*/
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    vector<int>arr ={1,2,3,4,5,6,7,8,9,10,200};
+    cout<<endl<<"The arrays : ";
+    for(int i: arr){
+        cout<<i<<" ";
+    } 
+
+    int el;
+    cout<<endl<<"Enter the element you want the upper bound for : ";
+    cin>>el;
+
+    int first = 0;
+    int last = arr.size()-1;
+    int ans = arr.size();
+    int middle = first +(last-first)/2;
+    while(first<=last){
+        middle = first+(last-first)/2;
+        if(arr[middle]>el){
+            ans = middle;
+            last = middle-1;
+        }
+        else if(arr[middle]<=el){
+            first = middle+1;
+        }
+    }
+    cout<<endl<<"The upper bound will be found at the index : "<<ans;
     return 0;
 }
