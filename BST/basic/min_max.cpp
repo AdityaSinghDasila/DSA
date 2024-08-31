5bee6fc1106a199a0275296a8dc449f8eb76b9a2
Aditya Singh <adityasinghdasila15@gmail.com>
2024-08-31 19:25:22 +0530

    max and min in a BST

--- BST/basic/min_max.cpp
+++ BST/basic/min_max.cpp
@@ -10,6 +10,34 @@ struct node{
         left = right =nullptr;
     }
 };
+
+void inOrder(node* root){
+    if(root == nullptr)
+        return;
+    inOrder(root->left);
+    cout<<root->val<<" ";
+    inOrder(root->right);
+}
+
+node* findMin(node* root,int& mini){
+
+    if(root==nullptr || root->left == nullptr){
+        if(root!=nullptr)
+            mini = min(mini,root->val); 
+        return root;
+    }
+    return findMin(root->left,mini);
+}
+
+node* findMax(node* root, int& maxi){
+    if(root == nullptr || root->right == nullptr){
+        if(root!=nullptr)   
+            maxi = max(maxi,maxi);
+        return root;
+    }
+    return findMax(root->right,maxi);
+}
+
 int main(){
     node* root = new node(8);
     root->left = new node(5);
@@ -20,5 +48,21 @@ int main(){
 
     root->right->left= new node(10);
     root->right->right = new node(14);
+
+    inOrder(root);
+
+    int mini = INT_MAX;
+    findMin(root,mini);
+    int maxi = INT_MIN; 
+    findMax(root,maxi);
+
+    if(mini)
+        cout<<endl<<"The minimum of bst : "<<mini;
+    else
+        cout<<endl<<"invalid bst";
+    if(maxi)
+        cout<<endl<<"The maximum of the bst : "<<maxi;
+    else  
+        cout<<endl<<"Invalid BST "<<endl;
     return 0;
 }
