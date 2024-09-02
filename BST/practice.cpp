8c76e359d86ccd40ba3df9fafeece656e71e733d
Aditya Singh <adityasinghdasila15@gmail.com>
2024-09-02 17:15:33 +0530

    practice bst basics

---
+++ BST/practice.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+struct node{
+    int val;
+    node* left;
+    node* right;
+    node(int val1){
+        val = val1;
+        left = right = nullptr;
+    }
+};  
+
+void inOrder(node* root){
+    if(root == nullptr)
+        return;
+    inOrder(root->left);
+    cout<<root->val<<" ";
+    inOrder(root->right);
+}
+
+node* ins(node* root, int n){
+    if(root == nullptr){
+        node* temp = new node(n);
+        return temp;
+    }
+    node* mover = root;
+    while(mover!=nullptr){
+        if(mover->val>n){
+            if(mover->left==nullptr){
+                node* temp = new node(n);
+                mover->left = temp;
+                break;
+            }
+            else
+                mover = mover ->left;
+        }
+        else{
+            if(mover->right == nullptr){
+                node* temp = new node(n);
+                mover ->right = temp;
+                break;
+            }else{
+                mover = mover->right;
+            }
+        }
+    }
+    return root;    
+}
+
+int main(){
+    
+    node* root = new node(8);
+    root->left = new node(5);
+    root->right = new node(12);
+
+    root->left->left = new node(4);
+    root->left->right = new node(7);
+
+    root->right->left= new node(10);
+    root->right->right = new node(14);
+
+    cout<<endl<<"The BST : ";
+    inOrder(root);
+
+    int n;
+    cout<<endl<<"Enter a node to insert  :";
+    cin>>n;
+    root = ins(root,n);
+    cout<<endl<<"BST after insertion : ";
+    inOrder(root);
+    
+    // cout<<endl<<"Enter a node to delete : ";
+    // cin>>n;
+    // root = delNode(root,n);
+    // cout<<endl<<"BST after deletion : ";
+    // inOrder(root);
+
+
+    return 0;
+}
