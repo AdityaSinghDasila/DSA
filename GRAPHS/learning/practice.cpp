41b2d0e8c1ec0fefb13ab21eacb464ac95f01159
Aditya Singh <adityasinghdasila15@gmail.com>
2024-09-07 17:54:36 +0530

    bfs traversal practice

--- GRAPHS/learning/practice.cpp
+++ GRAPHS/learning/practice.cpp
@@ -55,3 +55,55 @@ int main(){
     return 0;
 }
 */
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+void bfs_traverse(vector<int> adj[],int n, int x){
+    vector<int> visited(n+1,0);
+    queue<int> q;
+    q.push(x);
+    visited[x]=1;
+    while(!q.empty()){
+        int z = q.front();
+        cout<<z<<" ";
+        q.pop();
+        for(int i : adj[z]){
+            if(visited[i]==0){
+                q.push(i);
+                visited[i]=1;
+            }
+        }
+    }
+}
+int main(){
+    
+    //storing  graph
+    cout<<"Enter the number of nodes and edges : ";
+    int n,m;
+    cin>>n>>m;
+
+    vector<int> adj[n+1];
+    cout<<endl<<"Enter the (m) edges : ";
+    for(int i =0;i<m;i++){
+        int u,v;
+        cin>>u>>v;
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+    //now the graph has been stored
+
+    for(int i =1;i<n+1;i++){
+        cout<<i<<" : ";
+        for(int j : adj[i]){
+            cout<<j<<" ";
+        }
+        cout<<endl;
+    }
+    cout<<endl<<"Enter the starting node for the bfs and dfs traversal : ";
+    int x;
+    cin>>x;
+
+    bfs_traverse(adj,n,x);
+    
+    return 0;
+}
