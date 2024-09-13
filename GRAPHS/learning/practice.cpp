23a2b6bb77e094becd4d9facb58186b595becb19
Aditya Singh <adityasinghdasila15@gmail.com>
2024-09-13 09:28:34 +0530

    practice of bfs and dfs traversal of the graph

--- GRAPHS/learning/practice.cpp
+++ GRAPHS/learning/practice.cpp
@@ -125,7 +125,7 @@ int main(){
     return 0;
 }*/
 
-
+/*
 #include <iostream>
 #include <bits/stdc++.h>
 using namespace std;
@@ -191,6 +191,80 @@ int main(){
     vector<int> dfs_visit(n+1,0);
     cout<<endl<<"The DFS traversal of this graph : ";
     dfs_traverse(adj,n,x,dfs_visit);
+
+    return 0;
+}
+*/
+//APPLY BFS AND DFS in : 
+//1.adjacency list
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+void bfs_traverse(vector<vector<int>> adj, int x){
+    queue<int>q;
+    int size = adj.size();
+    vector<int> visited(size,0);
+    q.push(x);
+    visited[x]=1;
+    while(!q.empty()){
+        int N = q.front();
+        cout<<N<<" ";
+        q.pop();
+        for(int i : adj[N]){
+            if(visited[i]==0){
+                q.push(i);
+                visited[i]=1;
+            }
+        }
+    }
+}
+
+void dfs_traverse(vector<vector<int>> adj, int x, vector<int> & visited){
+    visited[x]=1;
+    cout<<x<<" ";
+    for(int i : adj[x]){
+        if(visited[i]==0){
+            dfs_traverse(adj,i,visited);
+        }
+    }
+}
+
+int main(){
+    cout<<"Enter the number of nodes and edges : ";
+    int n,m;
+    cin>>n>>m;
+    vector<vector<int>> adj(n+1,vector<int>(0)); //0 to n index , for if 0 to n-1 or from 1 to n.
+
+    cout<<endl<<"Enter the (m) edges : ";
+    for(int i =0;i<m;i++){
+        int u,v;
+        cin>>u>>v;
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+    
+    cout<<endl<<"The adjacency list : ";
+    for(int i =0;i<n+1;i++){
+        cout<<i<<" : ";
+        for(int j : adj[i]){
+            cout<<j<<" ";
+        }
+        cout<<endl;
+    }
+
+    cout<<endl<<"Enter the starting node for traversal : ";
+    int x;
+    cin>>x;
+
+    //now first for the bfs
+    cout<<endl<<"The BFS traversal of the graph : ";
+    bfs_traverse(adj,x);
+    
+    //since the DFS algo consists of recursion, we'll have to pass a visited list from the main() itself
+    vector<int> visited(n+1,0);
+    cout<<endl<<"The DFS traversal of the graph : ";
+    dfs_traverse(adj,x,visited);
 
     return 0;
 }
