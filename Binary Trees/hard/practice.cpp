4058f39d12e2a1f0937ffc9e4c6a214e2cc3bad0
Aditya Singh <adityasinghdasila15@gmail.com>
2024-11-19 17:25:59 +0530

    symmetrical binary tree or not

--- Binary Trees/hard/practice.cpp
+++ Binary Trees/hard/practice.cpp
@@ -67,6 +67,7 @@ int main(){
 
 
 //bottom view of a bt
+/*
 #include <iostream>
 #include <bits/stdc++.h>
 using namespace std;
@@ -134,6 +135,106 @@ int main(){
     for(int i : top){
         cout<<i<<" ";
     }
+
+    return 0;
+}
+*/
+
+//practice
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+class node{
+    public:
+    int val;
+    node* left;
+    node* right;
+    node(int val1){
+        val = val1;
+        left = right = nullptr;
+    }
+};
+
+void preOrder(node* root){
+    if(root == nullptr){
+        return;
+    }
+    cout<<root->val<<" ";
+    preOrder(root->left);
+    preOrder(root->right);
+}
+
+int findSymmetry(node* root, node* roott){
+    if(root==nullptr && roott == nullptr){
+        return 1;
+    }
+    if((root == nullptr && roott!=nullptr) || (roott == nullptr && root!=nullptr)){
+        cout<<endl<<"A whole node is missing!";
+        return -1;
+    }
+    int left = findSymmetry(root->left,roott->left);
+    int right = findSymmetry(root->right,roott->right);
+    if(left ==-1 || right ==-1){
+        return -1;
+    }
+    if(root->val != roott->val){
+        cout<<endl<<"Here we fed up "<<root->val<<" & "<<roott->val;
+        return -1;
+    }else{
+        return 1;
+    }
+    return 1;
+}
+
+int main(){
+
+    //first tree: 
+    node* root = new node(1);
+    root->left = new node(2);
+    root->right = new node(11);
+
+    root->left->left = new node(5);
+    root->left->right = new node(6);
+
+    root->right->left = new node(0);
+    root->right->right = new node(56);
+
+    root->left->right->right = new node(3);
+    
+    root->left->right->right->right = new node(2);
+    root->left->right->right->right->left = new node(97);
+
+
+    //second tree
+    node* roott = new node(1);
+    roott->left = new node(2);
+    roott->right = new node(11);
+
+    roott->left->left = new node(5);
+    roott->left->right = new node(6);
+
+    roott->right->left = new node(23);//here the tree is not the same, it is 0 in the above tree instead of 23
+    roott->right->right = new node(56);
+
+    roott->left->right->right = new node(3);
+    
+    roott->left->right->right->right = new node(2);
+    roott->left->right->right->right->left = new node(97);
+
+    //printing both trees :
+    cout<<endl<<"The 1st Binary tree preorder traversal : ";
+    preOrder(root);
+    cout<<endl<<"The 2nd Binary tree preorder traversal : ";
+    preOrder(roott);
+
+    int ans =1;
+    ans = findSymmetry(root,roott);
+    if(ans==-1){
+        cout<<endl<<"The two binary trees are NOT symmetrical!!";
+    }else{
+        cout<<endl<<"The two binary trees are SYMMETRICAL!!";
+    }
+
 
     return 0;
 }
