64182f82c2e7ee3c2c12c5cd34ec83d1a1cb31aa
Aditya Singh <adityasinghdasila15@gmail.com>
2025-02-03 12:21:24 +0530

    merge sort practice

--- practice Random/practice.cpp
+++ practice Random/practice.cpp
@@ -2004,3 +2004,68 @@ int main(){
     return 0;
 }
 */
+
+//merge sort
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+void merge(vector<int>& arr, int first,int middle, int last){
+    int i= first;
+    int j= middle+1;
+    vector<int> temp;
+
+    while(i<=middle && j <= last){
+        if(arr[i]<=arr[j]){
+            temp.push_back(arr[i]);
+            i++;
+        }else{
+            temp.push_back(arr[j]);
+            j++;
+        }
+    }
+    while(i<=middle){
+        temp.push_back(arr[i]);
+        i++;
+    }
+
+    while(j<=last){
+        temp.push_back(arr[j]);
+        j++;
+    }
+
+    //now that both iterators have been exhausted
+    for(int k = first;k <=last;k++){
+        arr[k] = temp[k-first];
+    }//now the contents have been transferred
+    
+}
+
+void mergeSort(vector<int>& arr, int first, int last){
+    if(first>=last){
+        return;
+    }
+    int middle = (first+last)/2;
+    mergeSort(arr,first,middle);
+    mergeSort(arr,middle+1,last);
+    merge(arr,first,middle,last);
+}
+
+int main(){
+
+    vector<int> arr = {9,2,45,2,1,67,87,45,32,12,5,1,0,4,33,45,6,7,89,9,3,5,4};
+    cout<<endl<<"The array before sorting : ";
+    for(int i : arr){
+        cout<<i<<" ";
+    }
+
+    mergeSort(arr,0,arr.size()-1);
+
+    cout<<endl<<"The array after sorting : ";    
+
+    for(int i : arr){
+        cout<<i<<" ";
+    }   
+
+    return 0;
+}
