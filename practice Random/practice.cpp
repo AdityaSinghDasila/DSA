9fc1e053c9fd8845fabca7a0073db6bee7b4727e
Aditya Singh <adityasinghdasila15@gmail.com>
2025-02-16 17:42:01 +0530

    merge sort practice

--- practice Random/practice.cpp
+++ practice Random/practice.cpp
@@ -2265,7 +2265,7 @@ int main(){
     return 0;
 }
 */
-
+/*
 
 #include <iostream>
 #include <bits/stdc++.h>
@@ -2497,6 +2497,68 @@ int main(){
     lca = findLca(root,x,y);
     cout<<endl<<"The lca : "<<lca;
     
+
+    return 0;
+}
+    */
+
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+
+void merge(vector<int>& arr, int first, int middle, int last){
+    int i = first;
+    int j = middle+1;
+    vector<int>temp;
+    while(i<=middle && j<=last){
+        if(arr[i]<=arr[j]){
+            temp.push_back(arr[i]);
+            i++;
+        }else{
+            temp.push_back(arr[j]);
+            j++;
+        }
+    }
+    while(i<=middle){
+        temp.push_back(arr[i]);
+        i++;
+    }
+    while(j<=last){
+        temp.push_back(arr[j]);
+        j++;
+    }
+
+    //now the temp contains the sorted stuff that is to be placed in arr from index first to last
+    for(int z = first; z<=last;z++){
+        arr[z] = temp[z-first];
+    }
+}
+
+void mergeSort(vector<int>& arr, int first, int last){
+    if(first >= last){
+        return;
+    }
+    int middle = (first+last)/2;
+    mergeSort(arr,first,middle);
+    mergeSort(arr,middle+1,last);
+    merge(arr,first,middle,last);
+}
+
+int main(){
+
+    vector<int> arr = {9,2,45,2,1,67,87,45,32,12,5,1,0,4,33,45,6,7,89,9,3,5,4};
+    cout<<endl<<"The array before sorting : ";
+    for(int i: arr){
+        cout<<i<<" ";
+    }
+    
+    mergeSort(arr,0,arr.size());
+
+    cout<<endl<<"The array after sorting : ";
+    for(int i:  arr){
+        cout<<i<<" ";
+    }
 
     return 0;
 }
